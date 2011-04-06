3d287ab6a5f3d50a154e28a79a3bed00ed1fc33f
Allison Randal <allison@lohutok.net>
2011-04-05 20:00:47 -0700

    Adding Google Analytics to comment review page

--- bin/process_comments.py
+++ bin/process_comments.py
@@ -96,7 +96,25 @@ def table_row(index, line, comments):
     return html
 
 def table_footer():
-    html = "</table>\n</div>\n</body>\n</html>\n"
+    html = """</table>
+</div>
+</body>
+</html>
+<script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-22549809-1']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
+</html>
+"""
     return html
 
 def write_html(filename, text, comments):
